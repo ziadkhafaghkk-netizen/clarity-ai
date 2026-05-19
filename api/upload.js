11b9efcd2949b4a33cb109b8bd66bffabe492ca5
ziadkhafaghkk-netizen <ziadkhafaghkk@gmail.com>
2026-05-19 13:52:35 +0300

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -3,9 +3,9 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore, doc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_KEY,
-  authDomain: process.env.FIREBASE_AUTH,
-  projectId: process.env.FIREBASE_PROJECT,
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
 };
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
