7cc3db88ac9a122e2e5d27521ea80449cff924f7
ziadkhafaghkk-netizen <ziadkhafaghkk@gmail.com>
2026-05-19 18:31:17 +0300

    Update firebase.js

--- app/lib/firebase.js
+++ app/lib/firebase.js
@@ -1,34 +1,13 @@
-"use client";
-import { useState } from "react";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { app } from "../lib/firebase";
+import { initializeApp } from "firebase/app";
 
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();
+const firebaseConfig = {
+  apiKey: "AIzaSyBwpgzE9QYr-H9nB82IDmDwwmwWHCzQs7Y",
+  authDomain: "clarity-ai-f9404.firebaseapp.com",
+  projectId: "clarity-ai-f9404",
+  storageBucket: "clarity-ai-f9404.firebasestorage.app",
+  messagingSenderId: "281058774281",
+  appId: "1:281058774281:web:1f7f21771d7ad7fe021973",
+  measurementId: "G-W2RDLJXBG6"
+};
 
-export default function LoginPage() {
-  const [msg, setMsg] = useState("");
-
-  const handleGoogleLogin = async () => {
-    try {
-      const result = await signInWithPopup(auth, provider);
-      const user = result.user;
-      setMsg(`أهلاً ${user.displayName}`);
-    } catch (err) {
-      setMsg(err.message);
-    }
-  };
-
-  return (
-    <div style={{ padding: "20px", textAlign: "center" }}>
-      <h2>تسجيل الدخول</h2>
-      <button 
-        onClick={handleGoogleLogin}
-        style={{ padding: "10px 20px", fontSize: "16px", cursor: "pointer" }}
-      >
-        تسجيل الدخول بجوجل
-      </button>
-      <p>{msg}</p>
-    </div>
-  );
-}
+export const app = initializeApp(firebaseConfig); ده الكود
